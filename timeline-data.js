9ca536fdb54e85a5d8c43077ab6e35ab5bca83c4
jasonkayzk <jasonkayzk@gmail.com>
2025-04-21 15:46:25 +0800

    fix: image

--- timeline-data.js
+++ timeline-data.js
@@ -1,9 +1,9 @@
 // 示例时间线数据
 const timelineData = [
-    { date: '2025-04-21', event: '新增 <a href="/blog-timeline">博客时间线</a> 页面入口', image: '/images/blog-timeline.png' },
-    { date: '2025-04-21', event: '新增 <a href="/photos">摄影分享</a> 页面入口', image: '/images/photos-entry.png' },
-    { date: '2025-03-20', event: '实时数据 <a href="/realtime">实时数据</a> 页面新增：<a href="https://us.umami.is/share/j5SVhZLblKH5HIbJ/jasonkayzk.github.io">umami</a> 站点数据分析', image: '/images/realtime-umami.png'  },
-    { date: '2024-08-26', event: '新增 <a href="/running/">跑步健身</a> 页面入口', image: '/images/running.png' },
+    { date: '2025-04-21', event: '新增 <a href="/blog-timeline">博客时间线</a> 页面入口', image: 'images/blog-timeline.png' },
+    { date: '2025-04-21', event: '新增 <a href="/photos">摄影分享</a> 页面入口', image: 'images/photos-entry.png' },
+    { date: '2025-03-20', event: '实时数据 <a href="/realtime">实时数据</a> 页面新增：<a href="https://us.umami.is/share/j5SVhZLblKH5HIbJ/jasonkayzk.github.io">umami</a> 站点数据分析', image: 'images/realtime-umami.png'  },
+    { date: '2024-08-26', event: '新增 <a href="/running/">跑步健身</a> 页面入口', image: 'images/running.png' },
     { date: '2024-07-11', event: '移除 <a href="/project/">张小凯のProject</a> 页面入口' },
     { date: '2024-07-11', event: '移除 <a href="/japanese/">日本語勉強教室</a> 页面入口' },
     { date: '2024-06-21', event: '关闭文件分享、幸运抽页面！' },
